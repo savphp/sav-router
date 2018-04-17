8b9b871b5f391bd6eb673e175ab7dae1fd1dad0d
jetiny <86267344@qq.com>
2018-04-17 09:21:54 +0800

    phpcs

--- spec/Router.spec.php
+++ spec/Router.spec.php
@@ -2,122 +2,122 @@
 
 use SavRouter\Router;
 
-describe("Router", function() {
-  
-  it("Router.build", function() {
-    $router = new Router();
-    $m = $router->build(array("name" => "Account"));
-    $a = $router->build(array("name" => "login", "modal" => "Account"));
-    expect($m)->toBeA('array');
-    expect($a)->toBeA('array');
-    expect($router->getRoutes())->toBeA('array');
-  });
+describe("Router", function () {
 
-  it('stripPrefix', function () {
-    expect(SavRouter\stripPrefix('', ''))->toEqual('');
-    expect(SavRouter\stripPrefix('/', ''))->toEqual('/');
-    expect(SavRouter\stripPrefix('/a', '/a'))->toEqual('/');
-    expect(SavRouter\stripPrefix('/a/', '/a'))->toEqual('/');
-    expect(SavRouter\stripPrefix('/a/b', '/a'))->toEqual('/b');
+    it("Router.build", function () {
+        $router = new Router();
+        $m = $router->build(array("name" => "Account"));
+        $a = $router->build(array("name" => "login", "modal" => "Account"));
+        expect($m)->toBeA('array');
+        expect($a)->toBeA('array');
+        expect($router->getRoutes())->toBeA('array');
+    });
 
-    expect(SavRouter\stripPrefix('/a/', '/a/'))->toEqual('/');
-    expect(SavRouter\stripPrefix('/a/b', '/a/'))->toEqual('/b');
-  });
+    it('stripPrefix', function () {
+        expect(SavRouter\stripPrefix('', ''))->toEqual('');
+        expect(SavRouter\stripPrefix('/', ''))->toEqual('/');
+        expect(SavRouter\stripPrefix('/a', '/a'))->toEqual('/');
+        expect(SavRouter\stripPrefix('/a/', '/a'))->toEqual('/');
+        expect(SavRouter\stripPrefix('/a/b', '/a'))->toEqual('/b');
 
-  it("Router.basic", function() {
-    $router = new Router(array(
-      'prefix' => '',
-      'caseType' => 'camel',
-      'method' => 'GET',
-      'sensitive' => true,
-    ));
-    $router->load(array("modals" => array(
-      "Home" => 
+        expect(SavRouter\stripPrefix('/a/', '/a/'))->toEqual('/');
+        expect(SavRouter\stripPrefix('/a/b', '/a/'))->toEqual('/b');
+    });
+
+    it("Router.basic", function () {
+        $router = new Router(array(
+        'prefix' => '',
+        'caseType' => 'camel',
+        'method' => 'GET',
+        'sensitive' => true,
+        ));
+        $router->load(array("modals" => array(
+        "Home" =>
         array("routes" => array(
-            "default" => array(), 
-            "relative" => array("path" => 'relativeRoute'), 
-            "absolute" => array("path" => '/absoluteRoute'), 
+            "default" => array(),
+            "relative" => array("path" => 'relativeRoute'),
+            "absolute" => array("path" => '/absoluteRoute'),
             "user" => array("path" => 'user/:id')
           )
         ),
-      "Article" => 
-        array("path" => 'art', 
+        "Article" =>
+        array("path" => 'art',
           "routes" => array(
-            "list" => array(), 
-            "cat" => array("path" => '/article/cat/:id'), 
+            "list" => array(),
+            "cat" => array("path" => '/article/cat/:id'),
             "item" => array("path" => 'item/:id')
           )
         )
-      )
-    ));
-    $pathEqual = function ($path, $end = false) use ($router){
-      $ret = $router->matchRoute($path, 'GET');
-      expect($ret)->toBeA('array');
-      expect($ret['route'])->toBeA('array');
-      if (is_string($end)) {
-        expect($ret['route']['path'])->toEqual($end);
-      } else {
-        expect($end ? ($ret['route']['path'] . '/') : $ret['route']['path'])->toEqual($path);
-      }
-    };
+        )
+        ));
+        $pathEqual = function ($path, $end = false) use ($router) {
+            $ret = $router->matchRoute($path, 'GET');
+            expect($ret)->toBeA('array');
+            expect($ret['route'])->toBeA('array');
+            if (is_string($end)) {
+                expect($ret['route']['path'])->toEqual($end);
+            } else {
+                expect($end ? ($ret['route']['path'] . '/') : $ret['route']['path'])->toEqual($path);
+            }
+        };
 
-    $pathEqual('/home/default');
-    $pathEqual('/home/default/', true);
-    $pathEqual('/home/relativeRoute');
-    $pathEqual('/home/relativeRoute/', true);
-    $pathEqual('/absoluteRoute');
-    $pathEqual('/absoluteRoute/', true);
-    $pathEqual('/home/user/1', '/home/user/:id');
-    $pathEqual('/home/user/1/', '/home/user/:id');
+        $pathEqual('/home/default');
+        $pathEqual('/home/default/', true);
+        $pathEqual('/home/relativeRoute');
+        $pathEqual('/home/relativeRoute/', true);
+        $pathEqual('/absoluteRoute');
+        $pathEqual('/absoluteRoute/', true);
+        $pathEqual('/home/user/1', '/home/user/:id');
+        $pathEqual('/home/user/1/', '/home/user/:id');
 
-  });
+    });
 
-  it("Router.sensitive", function() {
-    $router = new Router(array(
-      'caseType' => 'hyphen',
-      'method' => 'GET',
-      'sensitive' => false,
-    ));
-    $router->load(array("modals" => array(
-      "UserProfile" => 
+    it("Router.sensitive", function () {
+        $router = new Router(array(
+        'caseType' => 'hyphen',
+        'method' => 'GET',
+        'sensitive' => false,
+        ));
+        $router->load(array("modals" => array(
+        "UserProfile" =>
         array("routes" => array(
-            "HomeInfo" => array(), 
-            "UserAddress" => array("path" => 'UserAddress'), 
+            "HomeInfo" => array(),
+            "UserAddress" => array("path" => 'UserAddress'),
           )
         ),
-      )
-    ));
-    expect($router->matchRoute('/user-profile/home-info', 'GET'))->toBeA('array');
-    expect($router->matchRoute('/user-PROFILE/HOME-info/', 'GET'))->toBeA('array');
-    expect($router->matchRoute('/user-profile/HomeInfo', 'GET'))->toBe(null);
-    expect($router->matchRoute('/UserProfile/home-info', 'GET'))->toBe(null);
-    expect($router->matchRoute('/UserProfile/HomeInfo', 'GET'))->toBe(null);
-    expect($router->matchRoute('/user-profile/UserAddress', 'GET'))->toBeA('array');
-  });
+        )
+        ));
+        expect($router->matchRoute('/user-profile/home-info', 'GET'))->toBeA('array');
+        expect($router->matchRoute('/user-PROFILE/HOME-info/', 'GET'))->toBeA('array');
+        expect($router->matchRoute('/user-profile/HomeInfo', 'GET'))->toBe(null);
+        expect($router->matchRoute('/UserProfile/home-info', 'GET'))->toBe(null);
+        expect($router->matchRoute('/UserProfile/HomeInfo', 'GET'))->toBe(null);
+        expect($router->matchRoute('/user-profile/UserAddress', 'GET'))->toBeA('array');
+    });
 
-  it("Router.load", function() {
-    $router = new Router(array(
-      'caseType' => 'hyphen',
-      'method' => 'GET',
-      'sensitive' => false,
-    ));
-    $router->load(array(
-      "modals" => array(
+    it("Router.load", function () {
+        $router = new Router(array(
+        'caseType' => 'hyphen',
+        'method' => 'GET',
+        'sensitive' => false,
+        ));
+        $router->load(array(
+        "modals" => array(
         array("id" => 1, "name" => "UserProfile", "routes" => array(
           array("name" => "UserAddress")
         ))
-      ),
-      "actions" => array(
+        ),
+        "actions" => array(
         array("name" => "HomeInfo", "modal" => 1),
-      ),
-    ));
-    expect($router->matchRoute('/user-profile/home-info', 'GET'))->toBeA('array');
-    expect($router->matchRoute('/user-PROFILE/HOME-info/', 'GET'))->toBeA('array');
-    expect($router->matchRoute('/user-profile/HomeInfo', 'GET'))->toBe(null);
-    expect($router->matchRoute('/UserProfile/home-info', 'GET'))->toBe(null);
-    expect($router->matchRoute('/UserProfile/HomeInfo', 'GET'))->toBe(null);
-    expect($router->matchRoute('/user-profile/user-address', 'NONE'))->toBe(null);
-    expect($router->matchRoute('/user-profile/user-address', 'OPTIONS'))->toBeA('array');
-    expect($router->matchRoute('/user-profile/user-address', 'GET'))->toBeA('array');
-  });
+        ),
+        ));
+        expect($router->matchRoute('/user-profile/home-info', 'GET'))->toBeA('array');
+        expect($router->matchRoute('/user-PROFILE/HOME-info/', 'GET'))->toBeA('array');
+        expect($router->matchRoute('/user-profile/HomeInfo', 'GET'))->toBe(null);
+        expect($router->matchRoute('/UserProfile/home-info', 'GET'))->toBe(null);
+        expect($router->matchRoute('/UserProfile/HomeInfo', 'GET'))->toBe(null);
+        expect($router->matchRoute('/user-profile/user-address', 'NONE'))->toBe(null);
+        expect($router->matchRoute('/user-profile/user-address', 'OPTIONS'))->toBeA('array');
+        expect($router->matchRoute('/user-profile/user-address', 'GET'))->toBeA('array');
+    });
 });
--- src/Router.php
+++ src/Router.php
@@ -4,183 +4,196 @@ namespace SavRouter;
 use SavRouter\PathToRegexp;
 use SavUtil\CaseConvert;
 
-class Router {
-  public function __construct ($opts = array()) {
-    $this->opts = array(
-      "prefix" => '', 
-      "caseType" => 'camel', 
-      "sensitive" => true, 
-      "method" => 'POST'
-    );
-    foreach ($opts as $key => $value) {
-      $this->opts[$key] = $value;
-    }
-    $this->modalMap = array();
-    $this->modalRoutes = array();
-    $this->allRoutes = array();
-    $this->absoluteRoutes = createMethods(array());
-  }
-  public function getRoutes () {
-    return $this->allRoutes;
-  }
-  public function build($target) {
-    if (isset($target["modal"])) {
-      return $this->createActionRoute($target);
-    }
-    return $this->createModalRoute($target);
-  }
-  public function load($data) {
-    if (isset($data['modals'])) {
-      $modals = $data['modals'];
-      foreach ($modals as $key => $it) {
-        if (!is_numeric($key)) {
-          $it['name'] = $key;
+class Router
+{
+    public function __construct($opts = array())
+    {
+        $this->opts = array(
+        "prefix" => '',
+        "caseType" => 'camel',
+        "sensitive" => true,
+        "method" => 'POST'
+        );
+        foreach ($opts as $key => $value) {
+            $this->opts[$key] = $value;
         }
-        $this->build($it);
-      }
+        $this->modalMap = array();
+        $this->modalRoutes = array();
+        $this->allRoutes = array();
+        $this->absoluteRoutes = createMethods(array());
     }
-    if (isset($data['actions'])) {
-      foreach($data['actions'] as $it) {
-        $this->build($it);
-      }
+    public function getRoutes()
+    {
+        return $this->allRoutes;
     }
-  }
-  public function createModalRoute($opts) {
-    $route = array(
-      "name" => CaseConvert::pascalCase($opts['name']),
-      "path" => CaseConvert::convert($this->opts['caseType'], $opts['name']), 
-      "opts" => $opts, 
-      "keys" => array(), 
-      "childs" => createMethods(array())
-    );
-    $path = (isset($opts['path']) && is_string($opts['path'])) ? $opts['path'] : $route['path'];
-    $route['path'] = normalPath('/' . $path);
-    $route['regexp'] = PathToRegexp::convert($route['path'], $route['keys'], array(
-      "sensitive" => $this->opts['sensitive'], 
-      "end" => false)
-    );
-    $this->modalMap[$opts['name']] = &$route;
-    if (isset($opts['id'])) {
-      $this->modalMap[$opts['id']] = &$route;
-    }
-    $this->modalRoutes[] = &$route;
-    if (isset($opts['routes'])) {
-      foreach ($opts['routes'] as $key => $it) {
-        if (!is_numeric($key)) {
-          $it['name'] = $key;
+    public function build($target)
+    {
+        if (isset($target["modal"])) {
+            return $this->createActionRoute($target);
         }
-        $it['modal'] = $opts['name'];
-        $this->build($it);
-      }
+        return $this->createModalRoute($target);
     }
-    return $route;
-  }
-  public function createActionRoute($opts) {
-    $modal = &$this->modalMap[$opts['modal']];
-    if (isset($opts['method'])) {
-      $method = $opts['method'];
-    } else if (isset($modal['view'])) {
-      $method = !$modal['view'] ? 'GET' : $this->opts['method'];
-    } else {
-      $method = $this->opts['method'];
-    }
-    $route = array(
-      "name" => $modal['name'] . CaseConvert::pascalCase($opts['name']), 
-      "path" => CaseConvert::convert($this->opts['caseType'], $opts['name']), 
-      "opts" => $opts, 
-      "method" => $method, 
-      "modal" => &$modal, 
-      "keys" => array());
-    $isAbsolute = false;
-    $path = (isset($opts['path']) && is_string($opts['path'])) ? $opts['path'] : null;
-    if ($path && $path[0] === '/') {
-      $isAbsolute = true;
-    } else {
-      $path = $modal['path'] . '/' . ($path ? $path : $route['path']);
-    }
-    $route['path'] = normalPath($path);
-    $route['regexp'] = PathToRegexp::convert($route['path'], $route['keys'], array(
-      "sensitive" => $this->opts['sensitive'], "end" => true));
-    $route['isAbsolute'] = $isAbsolute;
-    if ($isAbsolute) {
-      $this->absoluteRoutes[$method][] = &$route;
-      $this->absoluteRoutes['ANY'][] = &$route;
-    } else {
-      $modal['childs'][$method][] = &$route;
-      $modal['childs']['ANY'][] = &$route;
-    }
-    $this->allRoutes[] = &$route;
-    return $route;
-  }
-  public function matchRoute($path, $method) {
-    $method = strtoupper($method);
-    if ($method === 'OPTIONS') {
-      $method = 'ANY';
-    }
-    if (array_search($method, static::$methods) === false) {
-      return;
-    }
-    $path = stripPrefix($path, $this->opts['prefix']);
-    $ret = array("path" => $path);
-    foreach ($this->absoluteRoutes[$method] as $route) {
-      if ($this->matchRouteItem($route, $path, $ret)) {
-        return $ret;
-      }
-    }
-    foreach ($this->modalRoutes as $route) {
-      if ($this->matchRouteItem($route, $path, $r)) {
-        foreach ($route['childs'][$method] as $subRoute) {
-          if ($this->matchRouteItem($subRoute, $path, $ret)) {
-            return $ret;
-          }
+    public function load($data)
+    {
+        if (isset($data['modals'])) {
+            $modals = $data['modals'];
+            foreach ($modals as $key => $it) {
+                if (!is_numeric($key)) {
+                    $it['name'] = $key;
+                }
+                $this->build($it);
+            }
         }
-      }
-    }
-  }
-  private function matchRouteItem($route, $path, &$ret) {
-    $mat = PathToRegexp::match($route['regexp'], $path);
-    if ($mat) {
-      if ($ret) {
-        $ret['route'] = $route;
-        $keys = $route["keys"];
-        $params = $ret["params"] = array();
-        $len = count($mat); 
-        for ($i = 1; $i < $len; ++$i) {
-          $key = $keys[$i - 1];
-          if ($key) {
-            $val = $mat[$i]; // @TODO 路径是否已经是解码后的数据
-            // $val = (gettype($mat[$i]) === 'string') ? urldecode($mat[$i]) : $mat[$i];
-            $params[$key['name']] = $val;
-          }
+        if (isset($data['actions'])) {
+            foreach ($data['actions'] as $it) {
+                $this->build($it);
+            }
         }
-      }
-      return true;
     }
-  }
-  public static $methods = array('GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'ANY');
+    public function createModalRoute($opts)
+    {
+        $route = array(
+        "name" => CaseConvert::pascalCase($opts['name']),
+        "path" => CaseConvert::convert($this->opts['caseType'], $opts['name']),
+        "opts" => $opts,
+        "keys" => array(),
+        "childs" => createMethods(array())
+        );
+        $path = (isset($opts['path']) && is_string($opts['path'])) ? $opts['path'] : $route['path'];
+        $route['path'] = normalPath('/' . $path);
+        $route['regexp'] = PathToRegexp::convert($route['path'], $route['keys'], array(
+            "sensitive" => $this->opts['sensitive'],
+            "end" => false)
+        );
+        $this->modalMap[$opts['name']] = &$route;
+        if (isset($opts['id'])) {
+            $this->modalMap[$opts['id']] = &$route;
+        }
+        $this->modalRoutes[] = &$route;
+        if (isset($opts['routes'])) {
+            foreach ($opts['routes'] as $key => $it) {
+                if (!is_numeric($key)) {
+                    $it['name'] = $key;
+                }
+                $it['modal'] = $opts['name'];
+                $this->build($it);
+            }
+        }
+        return $route;
+    }
+    public function createActionRoute($opts)
+    {
+        $modal = &$this->modalMap[$opts['modal']];
+        if (isset($opts['method'])) {
+            $method = $opts['method'];
+        } elseif (isset($modal['view'])) {
+            $method = !$modal['view'] ? 'GET' : $this->opts['method'];
+        } else {
+            $method = $this->opts['method'];
+        }
+        $route = array(
+            "name" => $modal['name'] . CaseConvert::pascalCase($opts['name']),
+            "path" => CaseConvert::convert($this->opts['caseType'], $opts['name']),
+            "opts" => $opts,
+            "method" => $method,
+            "modal" => &$modal,
+            "keys" => array()
+        );
+        $isAbsolute = false;
+        $path = (isset($opts['path']) && is_string($opts['path'])) ? $opts['path'] : null;
+        if ($path && $path[0] === '/') {
+            $isAbsolute = true;
+        } else {
+            $path = $modal['path'] . '/' . ($path ? $path : $route['path']);
+        }
+        $route['path'] = normalPath($path);
+        $route['regexp'] = PathToRegexp::convert($route['path'], $route['keys'], array(
+        "sensitive" => $this->opts['sensitive'], "end" => true));
+        $route['isAbsolute'] = $isAbsolute;
+        if ($isAbsolute) {
+            $this->absoluteRoutes[$method][] = &$route;
+            $this->absoluteRoutes['ANY'][] = &$route;
+        } else {
+            $modal['childs'][$method][] = &$route;
+            $modal['childs']['ANY'][] = &$route;
+        }
+        $this->allRoutes[] = &$route;
+        return $route;
+    }
+    public function matchRoute($path, $method)
+    {
+        $method = strtoupper($method);
+        if ($method === 'OPTIONS') {
+            $method = 'ANY';
+        }
+        if (array_search($method, static::$methods) === false) {
+            return;
+        }
+        $path = stripPrefix($path, $this->opts['prefix']);
+        $ret = array("path" => $path);
+        foreach ($this->absoluteRoutes[$method] as $route) {
+            if ($this->matchRouteItem($route, $path, $ret)) {
+                return $ret;
+            }
+        }
+        foreach ($this->modalRoutes as $route) {
+            if ($this->matchRouteItem($route, $path, $r)) {
+                foreach ($route['childs'][$method] as $subRoute) {
+                    if ($this->matchRouteItem($subRoute, $path, $ret)) {
+                        return $ret;
+                    }
+                }
+            }
+        }
+    }
+    private function matchRouteItem($route, $path, &$ret)
+    {
+        $mat = PathToRegexp::match($route['regexp'], $path);
+        if ($mat) {
+            if ($ret) {
+                $ret['route'] = $route;
+                $keys = $route["keys"];
+                $params = $ret["params"] = array();
+                $len = count($mat);
+                for ($i = 1; $i < $len; ++$i) {
+                    $key = $keys[$i - 1];
+                    if ($key) {
+                        $val = $mat[$i]; // @TODO 路径是否已经是解码后的数据
+                      // $val = (gettype($mat[$i]) === 'string') ? urldecode($mat[$i]) : $mat[$i];
+                        $params[$key['name']] = $val;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+    public static $methods = array('GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'ANY');
 }
 
-function createMethods($target) {
-  foreach(Router::$methods as $name) {
-    $target[$name] = array();
-  }
-  return $target;
-}
-
-function normalPath($path) {
-  return preg_replace('/\/+/', '/', $path);
-}
-
-function stripPrefix($src, $prefix) {
-  if ($prefix) {
-    $pos = strpos($src, $prefix);
-    if ($pos === 0 || $pos === 1 && $src[0] === '/') {
-      $src = substr($src, $pos + count($prefix), count($src));
-      if ($src[0] !== '/') {
-        $src = '/' + $src;
-      }
-      return $src;
+function createMethods($target)
+{
+    foreach (Router::$methods as $name) {
+        $target[$name] = array();
     }
-  }
-  return $src;
+    return $target;
+}
+
+function normalPath($path)
+{
+    return preg_replace('/\/+/', '/', $path);
+}
+
+function stripPrefix($src, $prefix)
+{
+    if ($prefix) {
+        $pos = strpos($src, $prefix);
+        if ($pos === 0 || $pos === 1 && $src[0] === '/') {
+            $src = substr($src, $pos + count($prefix), count($src));
+            if ($src[0] !== '/') {
+                $src = '/' + $src;
+            }
+            return $src;
+        }
+    }
+    return $src;
 }
